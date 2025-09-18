c4b3b24da30245f5b6db4aeeaa19b88c90338d82
Alex Adhikary <alexadhikaryofficial@gmail.com>
2025-09-19 03:04:51 +0600

    Authentication

---
+++ constants/index.ts
@@ -0,0 +1,250 @@
+
+export const resumes: Resume[] = [
+  {
+    id: "1",
+    companyName: "Google",
+    jobTitle: "Frontend Developer",
+    imagePath: "/images/resume_01.png",
+    resumePath: "/resumes/resume_1.pdf",
+    feedback: {
+      overallScore: 85,
+      ATS: {
+        score: 90,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 90,
+        tips: [],
+      },
+      content: {
+        score: 90,
+        tips: [],
+      },
+      structure: {
+        score: 90,
+        tips: [],
+      },
+      skills: {
+        score: 90,
+        tips: [],
+      },
+    },
+  },
+  {
+    id: "2",
+    companyName: "Microsoft",
+    jobTitle: "Cloud Engineer",
+    imagePath: "/images/resume_02.png",
+    resumePath: "/resumes/resume_2.pdf",
+    feedback: {
+      overallScore: 55,
+      ATS: {
+        score: 90,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 90,
+        tips: [],
+      },
+      content: {
+        score: 90,
+        tips: [],
+      },
+      structure: {
+        score: 90,
+        tips: [],
+      },
+      skills: {
+        score: 90,
+        tips: [],
+      },
+    },
+  },
+  {
+    id: "3",
+    companyName: "Apple",
+    jobTitle: "iOS Developer",
+    imagePath: "/images/resume_03.png",
+    resumePath: "/resumes/resume_3.pdf",
+    feedback: {
+      overallScore: 75,
+      ATS: {
+        score: 90,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 90,
+        tips: [],
+      },
+      content: {
+        score: 90,
+        tips: [],
+      },
+      structure: {
+        score: 90,
+        tips: [],
+      },
+      skills: {
+        score: 90,
+        tips: [],
+      },
+    },
+  },
+  {
+    id: "4",
+    companyName: "IBM",
+    jobTitle: "SQA Engineer",
+    imagePath: "/images/resume_04.png",
+    resumePath: "/resumes/resume_4.pdf",
+    feedback: {
+      overallScore: 74,
+      ATS: {
+        score: 82,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 77,
+        tips: [],
+      },
+      content: {
+        score: 70,
+        tips: [],
+      },
+      structure: {
+        score: 68,
+        tips: [],
+      },
+      skills: {
+        score: 74,
+        tips: [],
+      },
+    },
+  },
+  {
+    id: "5",
+    companyName: "Base64",
+    jobTitle: "Backend Developer",
+    imagePath: "/images/resume_05.jpg",
+    resumePath: "/resumes/resume_5.pdf",
+    feedback: {
+      overallScore: 89,
+      ATS: {
+        score: 92,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 93,
+        tips: [],
+      },
+      content: {
+        score: 85,
+        tips: [],
+      },
+      structure: {
+        score: 86,
+        tips: [],
+      },
+      skills: {
+        score: 88,
+        tips: [],
+      },
+    },
+  },
+  {
+    id: "6",
+    companyName: "NTGroup",
+    jobTitle: "Data Scientist",
+    imagePath: "/images/resume_06.jpg",
+    resumePath: "/resumes/resume_6.pdf",
+    feedback: {
+      overallScore: 91,
+      ATS: {
+        score: 95,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 87,
+        tips: [],
+      },
+      content: {
+        score: 91,
+        tips: [],
+      },
+      structure: {
+        score: 92,
+        tips: [],
+      },
+      skills: {
+        score: 88,
+        tips: [],
+      },
+    },
+  },
+];
+
+export const AIResponseFormat = `
+      interface Feedback {
+      overallScore: number; //max 100
+      ATS: {
+        score: number; //rate based on ATS suitability
+        tips: {
+          type: "good" | "improve";
+          tip: string; //give 3-4 tips
+        }[];
+      };
+      toneAndStyle: {
+        score: number; //max 100
+        tips: {
+          type: "good" | "improve";
+          tip: string; //make it a short "title" for the actual explanation
+          explanation: string; //explain in detail here
+        }[]; //give 3-4 tips
+      };
+      content: {
+        score: number; //max 100
+        tips: {
+          type: "good" | "improve";
+          tip: string; //make it a short "title" for the actual explanation
+          explanation: string; //explain in detail here
+        }[]; //give 3-4 tips
+      };
+      structure: {
+        score: number; //max 100
+        tips: {
+          type: "good" | "improve";
+          tip: string; //make it a short "title" for the actual explanation
+          explanation: string; //explain in detail here
+        }[]; //give 3-4 tips
+      };
+      skills: {
+        score: number; //max 100
+        tips: {
+          type: "good" | "improve";
+          tip: string; //make it a short "title" for the actual explanation
+          explanation: string; //explain in detail here
+        }[]; //give 3-4 tips
+      };
+    }`;
+
+
+export const prepareInstructions = ({
+                                      jobTitle,
+                                      jobDescription,
+                                      AIResponseFormat,
+                                    }: {
+  jobTitle: string;
+  jobDescription: string;
+  AIResponseFormat: string;
+}) =>
+  `You are an expert in ATS (Applicant Tracking System) and resume analysis.
+  Please analyze and rate this resume and suggest how to improve it.
+  The rating can be low if the resume is bad.
+  Be thorough and detailed. Don't be afraid to point out any mistakes or areas for improvement.
+  If there is a lot to improve, don't hesitate to give low scores. This is to help the user to improve their resume.
+  If available, use the job description for the job user is applying to to give more detailed feedback.
+  If provided, take the job description into consideration.
+  The job title is: ${jobTitle}
+  The job description is: ${jobDescription}
+  Provide the feedback using the following format: ${AIResponseFormat}
+  Return the analysis as a JSON object, without any other text and without the backticks.
+  Do not include any other text or comments.`;
